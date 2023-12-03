2a397bf7eb390f325d2a35da7df343f7fc1273f3
pebbletek <shirst@pebbletek.com.au>
2023-12-04 09:46:12 +1100

    Copied visguide3 to visguide as I'm happy then this is a good working baseline

--- visguide.py
+++ visguide.py
@@ -1,7 +1,13 @@
 import argparse
 import os
+from logging.handlers import SysLogHandler
+from dotenv import load_dotenv
+import cv2
+from PIL import Image
+import numpy as np
 import base64
 import time
+import subprocess
 import logging
 import errno
 import simpleaudio as sa
@@ -12,6 +18,7 @@ from elevenlabs import generate, play, set_api_key, voices
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose", action="store_true")
 parser.add_argument("-d", "--debug", action="store_true")
+parser.add_argument("-s", "--syslog", action="store_true")
 args = parser.parse_args()
 
 # Set the logging level based on the verbose and debug options
@@ -29,41 +36,170 @@ else:
 # Set the logging level based on the verbose and debug options
 logger = logging.getLogger()
 
+# If the syslog option is present, send logs to the syslog server
+if args.syslog:
+    syslog_handler = SysLogHandler(address=('splunk.local', 8516))
+    logger.addHandler(syslog_handler)
+
+
+# Define a function to check if the script is running on a Raspberry Pi
+def is_running_on_raspberry_pi():
+    try:
+        with open("/sys/firmware/devicetree/base/model", "r") as f:
+            if "Raspberry Pi" in f.read():
+                return True
+    except Exception:
+        return False
+
+# Conditional imports for Raspberry Pi specific modules
+if is_running_on_raspberry_pi():
+    import RPi.GPIO as GPIO
+    logger.info("Running on Raspberry Pi, GPIO module imported")
+else:
+    logger.info("Not running on Raspberry Pi, GPIO module not imported")
+
+# load the environment variables from the .env file if they are not set
+if 'OPENAI_API_KEY' not in os.environ or 'ELEVENLABS_API_KEY' not in os.environ or 'ELEVENLABS_VOICE_ID' not in os.environ:
+        # If not set, check for .env file and load it
+        dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+        if os.path.exists(dotenv_path):
+            load_dotenv(dotenv_path)
+            logger.info('.env file found and loaded')
+        else:
+            logger.warning('Required environment variables are not set and no .env file found')
+
+# Setup the button
+def button_callback(channel):
+    logger.info("Button was pushed!")
+    # Implement the action to be taken when the button is pressed
+
+# Setup GPIO Pins
+if is_running_on_raspberry_pi():
+    # Setup GPIO Pins only if on Raspberry Pi
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(17, GPIO.FALLING, callback=button_callback, bouncetime=200)
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+    logger.warning("Failed to open webcam")
+    raise IOError("Cannot open webcam")
+    exit(1)
+# Wait for the camera to initialize and adjust light levels
+time.sleep(2)
+
 # Create an OpenAI client
 client = OpenAI()
 
 # Set the ElevenLabs API key 
 set_api_key(os.environ.get("ELEVENLABS_API_KEY"))
 
-# Define a function to encode an image as base64
-def encode_image(image_path):
-    while True:
+def check_internet(timeout=60, max_response_time=30):  # Default timeout is 60 seconds, and default max_response_time is 30ms
+    hostname = "8.8.8.8"
+    start_time = time.time()
+
+    while time.time() - start_time < timeout:
+        # Use subprocess to get the ping statistics
         try:
-            with open(image_path, "rb") as image_file:
-                return base64.b64encode(image_file.read()).decode("utf-8")
-        except IOError as e:
-            if e.errno != errno.EACCES:
-                logger.error(f"IOError encountered: {e}")
-                raise
-            logger.debug("Waiting for file to become accessible...")
-            time.sleep(0.1)
+            ping_response = subprocess.check_output(
+                ["ping", "-c", "1", hostname],
+                stderr=subprocess.STDOUT,  # Redirect stderr to stdout
+                universal_newlines=True
+            )
+
+            # Extract the time from the ping response
+            ping_time = float(ping_response.split("time=")[1].split(" ms")[0])
+            if ping_time <= max_response_time:
+                logger.info(f"Internet connection detected with ping time: {ping_time} ms")
+                return True
+            else:
+                logger.warning(f"Slow internet response time: {ping_time} ms")
+                
+                # Play the user warning audio file
+                wave_obj = sa.WaveObject.from_wave_file("./assets/wav/slow_internet.wav")
+                play_obj = wave_obj.play()
+                play_obj.wait_done()
+                return True
+        except subprocess.CalledProcessError:
+            # This block is executed if the ping command fails
+            logger.warning("Ping command failed")
+
+        # Sleep for a short duration before retrying
+        time.sleep(1)
+
+    logger.warning("No internet connection detected within the given time frame")
+    # Play the user warning audio file
+    wave_obj = sa.WaveObject.from_wave_file("./assets/wav/No_internet.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+    return False
+
+
+def capture_image():
+    ret, frame = cap.read()
+    if ret:
+        # Convert the frame to a PIL image
+        pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+        # Resize the image
+        max_size = 250
+        ratio = max_size / max(pil_img.size)
+        new_size = tuple([int(x*ratio) for x in pil_img.size])
+        resized_img = pil_img.resize(new_size, Image.LANCZOS)
+
+        # Convert the PIL image back to an OpenCV image
+        frame = cv2.cvtColor(np.array(resized_img), cv2.COLOR_RGB2BGR)
+
+        # Encode the image as base64
+        frame_jpg = cv2.imencode('.jpg', frame)[1]
+        frame = base64.b64encode(frame_jpg).decode("utf-8")
+
+        # If debugging, save the frame as an image file
+        if args.debug:
+            # Create a folder to store the frames if it doesn't exist
+            folder = "frames"
+            if not os.path.exists(folder):
+                logger.debug("Creating folder to store frames")
+                os.makedirs(folder, exist_ok=True)
+            path = f"{folder}/frame.jpg"
+            logger.debug(f"Saving frame to {path}")
+            cv2.imwrite(path, frame_jpg)
+        # Delete the JPG version of the frame to save memory
+        del frame_jpg
+        # Return the base64 encoded image
+        return frame
+    else:
+        logger.warning("Failed to capture image")
+
 
 def play_audio(text):
     try:
+        # Split the text into the first sentence and the rest of the text
+        first_sentence, *rest = text.split(".")  # Split on the first period
+        
+        # Calls the ElevenLabs API to generate audio and the resulting WAV is the variable "audio"
         audio = generate(text, voice=os.environ.get("ELEVENLABS_VOICE_ID"))
 
-        unique_id = base64.urlsafe_b64encode(os.urandom(30)).decode("utf-8").rstrip("=")
-        dir_path = os.path.join("narration", unique_id)
-        os.makedirs(dir_path, exist_ok=True)
-        file_path = os.path.join(dir_path, "audio.wav")
-
-        with open(file_path, "wb") as f:
-            f.write(audio)
+        # If the debug option is set, save the audio to a file
+        if args.debug:
+            # Create a folder to store the audio if it doesn't exist
+            folder = "narration"
+            if not os.path.exists(folder):
+                logger.debug("Creating folder to store audio")
+                os.makedirs(folder, exist_ok=True)
+            path = f"{folder}/audio.wav"
+            logger.debug(f"Saving audio to {path}")
+            with open(path, 'wb') as f:
+                f.write(audio)
 
         play(audio)
     except Exception as e:
         logger.error(f"Error in play_audio: {e}")
 
+# FUNC: Generates the OpenAI "user" script
+# TODO: Explore if this an optimal prompt for each request.
 def generate_new_line(base64_image):
     return [
         {
@@ -78,11 +214,14 @@ def generate_new_line(base64_image):
         },
     ]
 
+# FUNC: Send image to OPENAI to get text summary back
 def analyze_image(base64_image, script):
     try:
         context = os.environ.get('CONTEXT', """
-            You are Sir David Attenborough. Narrate the picture of the human as if it is a nature documentary.
-            Make it snarky and funny. Don't repeat yourself. Make it short. If I do anything remotely interesting, make a big deal about it!
+            You are a guide for the blind. Describe the image with a focus on major features with a priority on risk. 
+            For example if there is a door ahead, is it opened or closed and how many paces is it. Another example would be where there is a road ahead, how many paces is it, is it busy or quiet. 
+            Dont repeat yourself and keep each description to 4 seconds.
+            Assume the image is always the view ahead.
         """)
         response = client.chat.completions.create(
             model="gpt-4-vision-preview",
@@ -108,13 +247,13 @@ def main():
 
     while True:
         try:
+            # Start the timers
             start_time = time.time()
-            image_path = os.path.join(os.getcwd(), "./frames/frame.jpg")
 
-            base64_image = encode_image(image_path)
-            timings['image_encoding'] += time.time() - start_time
+            # Capture the image
+            base64_image = capture_image()
 
-            logger.info("👀 VisGuide is watching...")
+            logger.info(" Sending image for narration ...")
             analysis_start_time = time.time()
             analysis = analyze_image(base64_image, script=script)
             timings['analysis'] += time.time() - analysis_start_time
@@ -132,10 +271,29 @@ def main():
         except Exception as e:
             logger.error(f"An error occurred in main loop: {e}")
             continue
+        except KeyboardInterrupt:
+            logger.info("Script interrupted by user, exiting gracefully.")
+            GPIO.cleanup()
+            cap.release()
+            cv2.destroyAllWindows()
+            exit(0)
 
     # Report timings
     for operation, time_taken in timings.items():
         logger.info(f"{operation}: {time_taken:.2f} seconds")
+
+
+    
+# Check for internet connectivity by pinging Google DNS
+while not check_internet(timeout=60, max_response_time=30):
+    logger.info("Waiting for internet connection...")
+    time.sleep(1)
+
+# Play audio file ./assets/visguide_is_ready.mp3 to indicate that VisGuide app is ready
+# load the mp3 audio file
+wave_obj = sa.WaveObject.from_wave_file("./assets/wav/VisGuide_is_ready.wav")
+# play the audio file
+play_obj = wave_obj.play()
 
 if __name__ == "__main__":
     main()
