971c3c2b0cc117b7bd107edec973476246f832a0
pebbletek <shirst@pebbletek.com.au>
2023-12-23 09:50:57 +1100

    test

--- testcam-simple.py
+++ testcam-simple.py
@@ -1,10 +1,23 @@
 import cv2
 import os
 import logging
-import keyboard
+import sys
+import select
+import threading
 
 # Setup logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.DEBUG, format=''%(asctime)s - %(levelname)s - %(message)s'')
+
+def non_blocking_input(stop_event):
+    while not stop_event.is_set():
+        if select.select([sys.stdin], [], [], 0.1)[0]:
+            line = sys.stdin.readline().strip()
+            if line == 'c':
+                logging.info("Captured keypress 'c'")
+                stop_event.set()
+            elif line == 'q':
+                logging.info("Captured keypress 'q'")
+                stop_event.set()
 
 def save_frame_camera_key(device_num, dir_path, basename, ext='jpg'):
     cap = cv2.VideoCapture(device_num)
@@ -17,31 +30,27 @@ def save_frame_camera_key(device_num, dir_path, basename, ext='jpg'):
     base_path = os.path.join(dir_path, basename)
 
     n = 0
-    logging.info("Press 'c' to capture a frame. Press 'q' to quit.")
+    stop_event = threading.Event()
+    input_thread = threading.Thread(target=non_blocking_input, args=(stop_event,))
+    input_thread.start()
 
-    while True:
-        try:
-            if keyboard.is_pressed('c'):  # Capturing the image
-                logging.debug("Detected 'c' keypress.")
-                ret, frame = cap.read()
-                if not ret:
-                    logging.error("Failed to read frame from camera.")
-                    break
+    logging.info("Type 'c' to capture a frame, followed by Enter. Type 'q' to quit, followed by Enter.")
 
+    while not stop_event.is_set():
+        ret, frame = cap.read()
+        if not ret:
+            logging.error("Failed to read frame from camera.")
+            break
+
+        if stop_event.is_set():
+            if sys.stdin.readline().strip() == 'c':
                 file_name = '{}_{}.{}'.format(base_path, n, ext)
                 cv2.imwrite(file_name, frame)
                 logging.info(f"Captured frame saved as {file_name}")
                 n += 1
-                while keyboard.is_pressed('c'):
-                    pass  # Wait for key release
-
-            if keyboard.is_pressed('q'):  # Quitting the program
-                logging.info("Detected 'q' keypress. Quitting.")
-                break
-        # Uncomment the following line to see any specific errors
-        except Exception as e:
-            logging.error(f"Error occurred: {e}")
-            break
+                stop_event.clear()
+                input_thread = threading.Thread(target=non_blocking_input, args=(stop_event,))
+                input_thread.start()
 
     # Properly release resources
     cap.release()
