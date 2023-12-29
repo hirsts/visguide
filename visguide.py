7ceae39914e1b467f455b5d9386074a9a08ea46d
pebbletek <shirst@pebbletek.com.au>
2023-12-30 10:15:19 +1100

    Added check for BT speaker to maine code

--- visguide.py
+++ visguide.py
@@ -99,7 +99,29 @@ def is_running_on_raspberry_pi():
     except Exception:
         logger.debug("TIMING:End TYPE:Func DESC:is_running_on_raspberry_pi() RESULT:False")
         return False
-    
+
+# FUNC: A function to get the list of connected Bluetooth devices
+def get_connected_devices():
+    try:
+        cmd = "bluetoothctl devices | cut -f2 -d' ' | while read uuid; do bluetoothctl info $uuid; done | grep -e 'Device\\|Connected\\|Name'"
+        result = subprocess.check_output(cmd, shell=True, text=True)
+        return result
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+        return ""
+
+# FUNC: A function to check if a specific Bluetooth device is connected
+def is_device_connected(device_name):
+    output = get_connected_devices()
+    devices = output.split("Device ")
+
+    for device in devices[1:]:  # Skip the first split as it's empty
+        if device_name in device:
+            return "Connected: yes" in device
+
+    return False
+
+
 def delete_frames(directory):
     for file in os.listdir(directory):
         file_path = os.path.join(directory, file)
@@ -128,6 +150,15 @@ if is_running_on_raspberry_pi():
     logger.debug("TIMING:Start TYPE:Action DESC:is_running_on_raspberry_pi() RESULT:True")
     import RPi.GPIO as GPIO
     logger.debug("TIMING:End TYPE:Action DESC:is_running_on_raspberry_pi() RESULT:RPi.GPIO imported")
+
+    # ACTION: Check if the Bluetooth device is connected
+    logger.debug("TIMING:Start TYPE:Action DESC:Check if Bluetooth device is connected RESULT:None")
+    device_name = "Jabra Speak 710"
+    # Loop until the device is connected
+    while not is_device_connected(device_name):
+        sys.stdout.write(f"\rDevice '{device_name}' is not connected. Checking again...")
+        sys.stdout.flush()  # Flush the buffer to ensure the output is displayed
+        time.sleep(1)  # Wait for 1 second before checking again
 else:
     import keyboard
     logger.debug("TIMING:End TYPE:Action DESC:is_running_on_raspberry_pi() RESULT:RPi.GPIO not imported. keyboard imported")
@@ -163,6 +194,7 @@ interrupt_main_process = False
 stop_audio_stream = False
 imagenum = 0
 voice_id = os.environ.get("ELEVENLABS_VOICE_ID")
+device_name = "Jabra Speak 710"
 logger.debug("TIMING:End TYPE:Action DESC:Define global variables RESULT:Done")
 
 # FUNC: Space Key press event handler
